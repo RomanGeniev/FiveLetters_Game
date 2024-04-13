2d9c97d741fa85eb1266038a8b1d05f01ca1706b
RomanGeniev <romangenievsky@yandex.ru>
2024-04-13 17:45:38 +0300

    jh

---
+++ src/main/java/com/example/fiveletters_game/data/Words.java
@@ -0,0 +1,7 @@
+package com.example.fiveletters_game.data;
+
+import java.util.List;
+
+public class Words {
+    protected static String[] words = new String[] {"актер", "амеба", "акция", "аллея", "армия"};
+}
