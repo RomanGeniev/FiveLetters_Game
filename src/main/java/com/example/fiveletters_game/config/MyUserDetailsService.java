a89c88342a161443b366b98292c867540d3332b1
RomanGeniev <romangenievsky@yandex.ru>
2024-05-02 14:45:25 +0300

    new

---
+++ src/main/java/com/example/fiveletters_game/config/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.fiveletters_game.config;
+
+import com.example.fiveletters_game.entity.User;
+import com.example.fiveletters_game.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByName(username);
+
+        return user.map(MyUserDetails::new)
+                .orElseThrow(()->new UsernameNotFoundException(username + "No users"));
+    }
+}
